3898b0b1e29ab661adf6820a0b9f8ae37104d22f
Jared Henry <henryjc@Jareds-MacBook-Pro.local>
2016-09-01 21:16:51 -0500

    Revamped the fab file

--- fabfile.py
+++ fabfile.py
@@ -3,56 +3,28 @@ from fabric.contrib.console import confirm
 
 # prepare for deployment
 
+def set_dev():
+    local('export APP_SETTINGS="project.server.config.DevelopmentConfig"')
+def set_prod():
+    local('export APP_SETTINGS="project.server.config.ProductionConfig"')
 
-def test():
-    with settings(warn_only=True):
-        result = local(
-            "python test_tasks.py -v && python test_users.py -v", capture=True
-        )
-    if result.failed and not confirm("Tests failed. Continue?"):
-        abort("Aborted at user request.")
+def create_db():
+    """
+    Initializes the creation of the DB.
+    """
+    local("python manage.py create_db")
+    local("python manage.py db init")
+    local("python manage.py db migrate")
+    local("python manage.py create_admin")
+    local("python manage.py create_data")
 
+def install_requirements():
+    local('pip install -r requirements.txt')
 
-def commit():
-    message = raw_input("Enter a git commit message: ")
-    local("git add . && git commit -am '{}'".format(message))
-
-
-def push():
-    local("git push origin master")
-
-
-def prepare():
-    test()
-    commit()
-    push()
-
-# deploy to heroku
+def setup_server():
+    local('apt-get update')
+    local('apt-get upgrade')
+    local('apt-get -y install python-dev python-pip git python-mysqldb')
+    install_requirements()
 
 
-def pull():
-    local("git pull origin master")
-
-
-def heroku():
-    local("git push heroku master")
-
-
-def heroku_test():
-    local(
-        "heroku run python test_tasks.py -v && heroku run python test_users.py -v"
-    )
-
-
-def deploy():
-    pull()
-    test()
-    commit()
-    heroku()
-    heroku_test()
-
-# rollback
-
-
-def rollback():
-    local("heroku rollback")
