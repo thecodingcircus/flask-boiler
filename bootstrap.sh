19548f3409e18c08a29a33649b665fc4008b33cb
Jared Henry <henryjc@Jareds-MacBook-Pro.local>
2016-09-05 12:46:13 -0500

    Added MySQL, MongoDB to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y python-dev python-pip git fabric python-mysqldb libffi-dev
+sudo apt-get update
+sudo apt-get install -y python-dev python-pip git fabric python-mysqldb libffi-dev
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server redis-server
 mkdir -p /opt/projects
 cd /opt/projects
 git clone https://jaredhenry6@bitbucket.org/jaredhenry6/flask-boiler.git
 cd flask-boiler
 pip install -r requirements.txt
+pip install pymongo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
 
 
